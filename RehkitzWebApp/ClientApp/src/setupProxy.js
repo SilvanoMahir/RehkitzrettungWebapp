1317ac2a2581a9b75e2b9a7483cc21423fff4722
Silvano Stecher <silvano.stecher@gmail.com>
2023-05-16 22:47:17 +0200

    react runs but fetching data not working

---
+++ RehkitzWebApp/ClientApp/src/setupProxy.js
@@ -0,0 +1,16 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/api/protocols",
+    "/api/regions",
+    "/api/users",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:7200',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
---
+++ RehkitzWebApp/User.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Models;
+
+public class User
+{
+
+    [Key]
+    public int userId { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string? userFirstName { get; set; }
+
+    [Column(TypeName = "nvarchar(50)")]
+    public string? userLastName { get; set; }
+    
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string? userRole { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string? userMail { get; set; }
+
+}
