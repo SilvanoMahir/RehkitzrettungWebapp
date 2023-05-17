9ccea6c2c345797f0a0ff3ecc808ad8cb08f91f2
Silvano Stecher <silvano.stecher@gmail.com>
2023-05-17 18:47:53 +0200

    working now with DB access, clean up tbd

--- RehkitzWebApp/Program.cs
+++ RehkitzWebApp/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+//Dependency Injection of DBContext
+builder.Services.AddDbContext<ApiDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
 
 var app = builder.Build();
 
