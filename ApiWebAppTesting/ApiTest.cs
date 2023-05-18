ae2a8bd8b747cbfe60f565af6014b6f54ada8110
Silvano Stecher <silvano.stecher@gmail.com>
2023-05-18 11:30:04 +0200

    first API tests implemented accesing Azure DB

---
+++ ApiWebAppTesting/ApiTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApiWebAppTesting
+{
+    [TestClass]
+    public class ApiTest
+    {
+        private HttpClient _httpClient;
+
+        string expectedProtocolsResult = "[{\"protocolId\":1,\"protocolCode\":\"GR-0024\",\"clientFullName\":\"Hans Pua\",\"localName\":\"Chomps\",\"pilotFullName\":\"Johannes Erny\"," +
+            "\"regionName\":\"Sent\",\"remark\":\"Keine Bemerkung\",\"areaSize\":\">1ha\",\"foundFawns\":1,\"injuredFawns\":0,\"markedFawns\":0,\"date\":\"2023-05-07T12:00:00\"}," +
+            "{\"protocolId\":2,\"protocolCode\":\"GR-0023\",\"clientFullName\":\"Mark Smith\",\"localName\":\"Uina\",\"pilotFullName\":\"John Kane\"," +
+            "\"regionName\":\"Scuol\",\"remark\":\"Keine Bemerkung\",\"areaSize\":\"<1ha\",\"foundFawns\":2,\"injuredFawns\":1,\"markedFawns\":0,\"date\":\"2023-05-07T12:00:00\"}]";
+
+        public ApiTest() {
+            var webAppFactory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.UseEnvironment("Development");
+            });
+            _httpClient = webAppFactory.CreateDefaultClient();
+        }
+
+        [TestMethod]
+        public async Task getProtocolsTest()
+        {
+            var response = await _httpClient.GetAsync("/api/protocols");
+            var stringResult = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(expectedProtocolsResult, stringResult);
+        }
+    }
+}
--- local-db/db-queries/FillProtocolsTable.sql
+++ local-db/db-queries/FillProtocolsTable.sql
@@ -1,3 +1,6 @@
+USE [rehkitzrettung-db]
+GO
+
 INSERT INTO [dbo].[Protocol]
            ([protocolCode]
            ,[clientFullName]
