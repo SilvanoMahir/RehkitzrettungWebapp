c0ce27a718493662b357dfce51eb925885a64413
KristianKüttelNotebook <kriskuett@gmail.com>
2023-05-31 19:56:30 +0200

    Frontend connects to local db successfully (if set to local db in appsettings.json, however, strangely all 20 sec a new command prompt window opens when app is running)

--- RehkitzWebApp/ClientApp/src/index.tsx
+++ RehkitzWebApp/ClientApp/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ProtocolProvider} from './store/context'
-import * as React from 'react'
-
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
