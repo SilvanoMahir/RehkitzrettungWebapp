1db7fceaea6ba0ea0485d7724bd6f58b1f354c10
KristianKüttelNotebook <kriskuett@gmail.com>
2023-06-04 14:42:24 +0200

    Add typo in frontend test to check if pipeline fails in azure devops

--- RehkitzWebApp/ClientApp/src/App.test.tsx
+++ RehkitzWebApp/ClientApp/src/App.test.tsx
@@ -6,6 +6,6 @@ test('renders Rehkitzrettung app', () => {
   render(<MemoryRouter>
             <App />
          </MemoryRouter>)
-  const linkElement = screen.getByText(/Willkommen zur Rehkitzrettung App/i)
+  const linkElement = screen.getByText(/Willkommen zur Rekitzrettung App/i)
   expect(linkElement).toBeInTheDocument()
 })
