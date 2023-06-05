48cacd911490c3e11b13b16cd1dc594fd4fc6a78
KristianKüttelNotebook <kriskuett@gmail.com>
2023-06-05 23:27:08 +0200

    Rename ClientApp directory to clientapp to fix build error

---
+++ RehkitzWebApp/clientapp/src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import RescueListPage from './components/pages/RescueListPage'
+
+export const ROUTE_RESCUE_LIST_PAGE = '/' // TODO: Change this route to '/rescuelist' in near future
+
+export default function App() {
+    return (
+        <Routes>
+            <Route path={ROUTE_RESCUE_LIST_PAGE} element={<RescueListPage />} />
+        </Routes>
+    )
+}
