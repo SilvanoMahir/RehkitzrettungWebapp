5358c551d02dd2d585cfa1f9f75a490634d190ac
Silvano Stecher <silvano.stecher@gmail.com>
2023-07-09 14:46:28 +0200

    added map page

---
+++ RehkitzWebApp/clientapp/src/components/pages/MapPage.tsx
@@ -0,0 +1,42 @@
+
+import Iframe from 'react-iframe'
+import styled from 'styled-components'
+import { useMediaQuery } from 'react-responsive'
+import Sidebar from '../widgets/Sidebar/Sidebar'
+import { Menu } from '../widgets/Menu'
+
+
+export default function RescueListPage() {
+    const isNotMobile = useMediaQuery({ query: '(min-width: 426px)' })
+
+    return (
+        <RescueListLayout>
+            {!isNotMobile && <Menu/>}
+                <RescueListRowLayout>
+                    {(isNotMobile) && <Sidebar showSidebar={isNotMobile} />}
+                    <MapBlock>
+                        <Iframe url="https://geogr.mapplus.ch/viewer/geogr/"
+                        height="100%"
+                        width="100%"
+                        id=""
+                        className=""
+                        display="block"
+                        position="relative"/>
+                    </MapBlock>
+                </RescueListRowLayout>
+        </RescueListLayout>
+    )
+}
+
+const MapBlock = styled.div`
+    width: 100%
+`
+
+const RescueListLayout = styled.div`
+    height: 100%;
+`
+
+const RescueListRowLayout = styled.div`
+    display: flex;
+    height: 100%;
+`
