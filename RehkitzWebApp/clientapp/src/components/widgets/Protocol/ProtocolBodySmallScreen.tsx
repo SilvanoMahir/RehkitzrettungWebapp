ffe364bd5b98e46afa42ef891d5190cb032d0980
Silvano Stecher <silvano.stecher@gmail.com>
2023-07-12 19:34:40 +0200

    fixed UTF-8

--- RehkitzWebApp/clientapp/src/components/widgets/Protocol/ProtocolBodySmallScreen.tsx
+++ RehkitzWebApp/clientapp/src/components/widgets/Protocol/ProtocolBodySmallScreen.tsx
@@ -1,4 +1,4 @@
-﻿import styled from "styled-components"
+import styled from "styled-components"
 import { ProtocolEntries } from "../../../models/ProtocolEntries"
 import ProtocolEntry from './ProtocolEntry'
 
--- RehkitzWebApp/clientapp/src/components/widgets/Protocol/ProtocolEntryForAdaptPage.tsx
+++ RehkitzWebApp/clientapp/src/components/widgets/Protocol/ProtocolEntryForAdaptPage.tsx
@@ -1,4 +1,4 @@
-﻿import styled from "styled-components"
+import styled from "styled-components"
 import { TextInputForAdaptPage } from "../../controls/TextInput"
 
 interface Props {
