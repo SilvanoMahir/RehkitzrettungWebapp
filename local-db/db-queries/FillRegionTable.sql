b9f612c98d24a3c0a4ad2000103fd7e8cc328941
KristianKüttelNotebook <kriskuett@gmail.com>
2023-08-16 19:05:20 +0200

    Correct ContactPersonEmail in FillRegionTable.sql and FillUserAndRegionTable.sql

--- local-db/db-queries/FillRegionTable.sql
+++ local-db/db-queries/FillRegionTable.sql
@@ -10,7 +10,7 @@ INSERT INTO [rehkitzrettung-db].[dbo].[Region]
 			, [ContactPersonLastName]
 			, [ContactPersonEmail])
 VALUES
-    ('Tasna', 'GR', 'Bezirk 10', 0, 'Hans','Schmid', 'admin@tasna.com'),
-    ('Valsot', 'GR','Bezirk 10', 0, 'Urs', 'Locher', 'admin@valsot.com');
+    ('Tasna', 'GR', 'Bezirk 10', 0, 'Hans','Schmid', 'admin@tasna.ch'),
+    ('Valsot', 'GR','Bezirk 10', 0, 'Urs', 'Locher', 'admin@valsot.ch');
 
 GO
