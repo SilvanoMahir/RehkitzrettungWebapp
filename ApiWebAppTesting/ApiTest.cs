e98b85bc415cc61b8649b1e0bec215923f314a5a
KristianKüttelNotebook <kriskuett@gmail.com>
2023-09-17 21:37:18 +0200

    Refactor deleteProtocolTest()

--- ApiWebAppTesting/ApiTest.cs
+++ ApiWebAppTesting/ApiTest.cs
@@ -90,65 +90,15 @@ namespace ApiWebAppTesting
         }
 
         [TestMethod]
-        public async Task deleteProtocolTest()
+        public void deleteProtocolTest()
         {
+            registerNewAdminClientAsync().Wait();
+            var responseLogin = loginAsAdminClientAsync();
+            var token = getTokenAsync(responseLogin.Result).Result;
+
             int protocolIdToRemove = 2;
-
-            var user = new
-            {
-                userName = "admin_test",
-                userEmail = "admin@tasna.ch",
-                userPassword = "Password@123",
-                userDefinition = "Admin 1",
-                userFirstName = "Silvano",
-                userLastName = "Stecher",
-                userRegion = "Tasna"
-            };
-
-            var userLogin = new
-            {
-                username = "admin_test",
-                password = "Password@123"
-            };
-
-            // create a new admin user
-            string jsonPayload = JsonConvert.SerializeObject(user);
-            var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
-            var responseRegister = await _httpClient.PostAsync("/api/authenticate/register-admin", content);
-
-            // login as admin user
-            string jsonLoginPayload = JsonConvert.SerializeObject(userLogin);
-            var contentLogin = new StringContent(jsonLoginPayload, Encoding.UTF8, "application/json");
-            var responseLogin = await _httpClient.PostAsync("/api/authenticate/login", contentLogin);
-
-            // delete the protocol
-            string responseString = await responseLogin.Content.ReadAsStringAsync();
-            var responseJson = JObject.Parse(responseString);
-            string token = responseJson["token"].Value<string>();
-
-            // setup request for deleting one protocol
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Delete,
-                RequestUri = new Uri("/api/protocols/" + protocolIdToRemove, UriKind.Relative)
-            };
-            request.Headers.Add("Authorization", "Bearer " + token);
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            await _httpClient.SendAsync(request);
-
-            // setup request for getting the protocols
-            request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("/api/protocols", UriKind.Relative)
-            };
-            request.Headers.Add("Authorization", "Bearer " + token);
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            // send request
-            var responseGet = await _httpClient.SendAsync(request);
-            var stringResult = await responseGet.Content.ReadAsStringAsync();
+            sendDeleteProtocolsRequestAsync(token, protocolIdToRemove).Wait();
+            var stringResult = sendGetProtocolsRequestAsync(token).Result;
 
             // get the comparasion object from initialisation db and delete the desired object
             List<Protocol> protocolList = models.getProtocolExpectedResultList().ToList();
@@ -616,6 +566,19 @@ namespace ApiWebAppTesting
             request.Headers.Add("Authorization", "Bearer " + token);
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             request.Content = new StringContent(JsonConvert.SerializeObject(protocolDtoToUpdate), Encoding.UTF8, "application/json");
+
+            await _httpClient.SendAsync(request);
+        }
+
+        private async Task sendDeleteProtocolsRequestAsync(string token, int protocolIdToRemove)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri("/api/protocols/" + protocolIdToRemove, UriKind.Relative)
+            };
+            request.Headers.Add("Authorization", "Bearer " + token);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             await _httpClient.SendAsync(request);
         }
