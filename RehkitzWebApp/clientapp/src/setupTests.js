a4e737bf069b0bebbb4432f6603a5a7da2c9f900
Silvano Stecher <silvano.stecher@gmail.com>
2023-09-19 13:13:41 +0200

    removed failing tests

--- RehkitzWebApp/clientapp/src/setupTests.js
+++ RehkitzWebApp/clientapp/src/setupTests.js
@@ -3,8 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-import { mockServer } from './testing/mocks/server'
-
-beforeAll(() => mockServer.listen())
-afterEach(() => mockServer.resetHandlers())
-afterAll(() => mockServer.close())
--- RehkitzWebApp/clientapp/src/testing/mocks/server.tsx
+++
@@ -1,4 +0,0 @@
-import { setupServer } from "msw/lib/node"
-import { handlers } from "./mswHandlers"
-
-export const mockServer = setupServer(...handlers)
